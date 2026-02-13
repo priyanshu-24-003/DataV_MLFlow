2e6fcf4127083150015f5b5b851cc3b67354a2b1
Priyanshu <priyanshu24003@gmail.com>
2026-02-13 19:03:48 +0530

    c4, everything works fine as of now

--- src/A_data_injestion.py
+++ src/A_data_injestion.py
@@ -91,7 +91,7 @@ def main():
     df = load_data('data/src/palmer_penguins.csv')
     df = basic_processing(df)
 
-    XY, xy = train_test_spliter(df, 0.2, 42)
+    XY, xy = train_test_spliter(df, 0.3, 42)
 
     save_data(XY, xy, './data/raw')
 
